d8b50073f3326193a073d4b53cc2e98d853e65c1
DESKTOP-1IB2BBC\Miki <savamichael@hotmail.com>
2020-10-30 11:23:22 +0100

    FIxed Error  in MainPage.xaml

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Windows.Input;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,9 +16,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace GoldStarr_Trading
 {
     /// <summary>
@@ -40,9 +38,10 @@ namespace GoldStarr_Trading
 
         }
 
-        private void AddButtonCommandExecute()
+        private async void AddButtonCommandExecute()
         {
-
+            var dialog = new MessageDialog("Hi!");
+            await dialog.ShowAsync();
         }
 
         private void PopulateCustomerComboBox(StoreClass store)
