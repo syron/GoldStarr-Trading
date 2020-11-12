4060eafbb7585f005f520e6b0fa2c5031310188e
DESKTOP-1IB2BBC\Miki <savamichael@hotmail.com>
2020-11-12 10:52:28 +0100

    Implementation of IMessageToUser interface

---
+++ Interfaces/IMessageToUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldStarr_Trading.Classes
+{
+    interface IMessageToUser
+    {
+        Task MessageToUser(string inputMessage);
+    }
+}
